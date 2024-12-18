956f7c8649542e54ce0c91bb79174ac05ded1c5b
hudamnhd <muhamadhuda519@gmail.com>
2024-12-18 23:41:17 +0700

    - init rr7

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,17 @@
-import type { RouteConfig } from "@react-router/dev/routes";
+import {
+	index,
+	route,
+	layout,
+	type RouteConfig,
+} from "@react-router/dev/routes";
 
-export default [] satisfies RouteConfig;
+export default [
+	layout("layout/contacts.tsx", [
+		index("routes/home.tsx"),
+		route("contacts", "routes/contacts.tsx", [
+			route(":contactId", "routes/contacts/details.tsx"),
+			route(":contactId/destroy", "routes/contacts/destroy.tsx"),
+			route(":contactId/edit", "routes/contacts/edit.tsx"),
+		]),
+	]),
+] satisfies RouteConfig;
